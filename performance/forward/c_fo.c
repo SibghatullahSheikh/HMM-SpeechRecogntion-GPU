41e432a1828979a8a3d3192d84f589a9d9cf1e37
Leiming Yu <leimingyu830@gmail.com>
2013-10-01 11:08:39 -0400

    test forward

--- performance/forward/c_fo.c
+++ performance/forward/c_fo.c
@@ -8,48 +8,72 @@
 
 #define BUFSIZE 1000000
 
-float **b;
-float **a;
-float *pri;
+//float **b;
+//float **a;
+//float *pri;
+
+typedef struct{
+	float **b;
+	float **a;
+	float *pri;
+	int len;
+	int nstates;
+} HMM;
 
 
 //void read_config(char **files,int job, float **b, float **a, float *pri, int len);
-void read_config(char **files,int job,int len);
+void read_config(HMM* word, char **files,int job,int len);
 
 int getLineNum(char *file);
 
 void copy_2d(float **from_array, float **to_array, int row, int col);
 
-void read_2d(char *file ,float **x, int row ,int col);
+void read_a(char *file , HMM* word, int row ,int col);
 
-void read_1d(char *file ,float *x, int len);
+void read_b(char *file , HMM* word, int row ,int col);
 
-void check_2d_f(float **array, int row, int col);
+void read_pri(char *file ,HMM* word, int len);
 
-void check_1d_f(float *array, int len);
+void check_a(HMM *word);
+
+void check_b(HMM *word);
+
+void check_pri(HMM *word);
+
+void free_hmm(HMM *word);
+
+
+
+
+
+
 
 //void read_config(char **files,int job, float **b, float **a, float *pri, int len)
-void read_config(char **files,int job, int len)
+void read_config(HMM* word, char **files,int job, int len)
 {
 	// variables
 	int i;
 	int N = len;
 
+	word->nstates = N;
+	word->len = N;
+
+
 	// allocate parameters
-	//float **b;
-	b = (float**)malloc(sizeof(float*)*N);
+	//float **b = word->b;
+	word->b = (float**)malloc(sizeof(float*)*N);
 	for(i = 0; i < N; ++i) {
-		b[i] = (float*)malloc(sizeof(float)*N);
+		word->b[i] = (float*)malloc(sizeof(float)*N);
 	}
 
-	//float **a;
-	a = (float**)malloc(sizeof(float*)*N);
+	//float **a = word->a;
+	word->a = (float**)malloc(sizeof(float*)*N);
 	for(i = 0; i < N; ++i) {
-		a[i] = (float*)malloc(sizeof(float)*N);
+		word->a[i] = (float*)malloc(sizeof(float)*N);
 	}
 
-	//float *pri;
-	pri = (float*)malloc(sizeof(float)*N);
+	//float *pri = word->pri;
+	word->pri = (float*)malloc(sizeof(float)*N);
 
 
 	for(i = 0; i < 3; ++i){
@@ -58,21 +82,21 @@ void read_config(char **files,int job, int len)
 		// read B
 		if(i == 0){
 			printf("read B[%d][%d] \t\t", N, N);
-			read_2d(files[job*3],b,N,N);
+			read_b(files[job*3],word,N,N);
 			printf("done!\n");
 		}
 
 		// read A
 		if(i == 1){
 			printf("read A[%d][%d] \t\t", N, N);
-			read_2d(files[job*3+1],a,N,N);
+			read_a(files[job*3+1],word,N,N);
 			printf("done!\n");
 		}
 
 		// read prior
 		if(i == 2){
 			printf("read prior[%d] \t\t", N);
-			read_1d(files[job*3+2],pri,N);
+			read_pri(files[job*3+2],word,N);
 			printf("done!\n");
 		}
 
@@ -101,7 +125,7 @@ int getLineNum(char *file)
 }
 
 
-void read_1d(char *file ,float *x, int len)
+void read_pri(char *file ,HMM* word, int len)
 {
 	int lineNum = 0;
 	char buf[100];
@@ -114,7 +138,7 @@ void read_1d(char *file ,float *x, int len)
 	}else{
 		while(fgets(buf, sizeof(buf), fr) != NULL)
 		{
-			sscanf(buf,"%f", &x[lineNum]);
+			sscanf(buf,"%f", &word->pri[lineNum]);
 			lineNum++;
 		}
 		fclose(fr);
@@ -123,7 +147,7 @@ void read_1d(char *file ,float *x, int len)
 
 
 
-void read_2d(char *file ,float **x, int row ,int col)
+void read_b(char *file , HMM* word, int row ,int col)
 {
 	int i;
 	int lineNum = 0;
@@ -142,7 +166,7 @@ void read_2d(char *file ,float **x, int row ,int col)
 			pch =  strtok(buf," ");
 			while(pch != NULL)
 			{
-				sscanf(pch,"%f", &x[lineNum][i]);
+				sscanf(pch,"%f", &word->b[lineNum][i]);
 				i++;
 				pch = strtok (NULL, " ");
 			}
@@ -156,6 +180,37 @@ void read_2d(char *file ,float **x, int row ,int col)
 }
 
 
+void read_a(char *file , HMM* word, int row ,int col)
+{
+	int i;
+	int lineNum = 0;
+	char buf[BUFSIZE];
+	char *pch;
+	
+	FILE *fr;
+	fr = fopen(file, "r");
+	if(fr == NULL) {
+		printf("Can't open %s!\n", file);
+		exit(1);
+	}else{
+		while(fgets(buf, BUFSIZE, fr) != NULL)
+		{
+			i = 0;
+			pch =  strtok(buf," ");
+			while(pch != NULL)
+			{
+				sscanf(pch,"%f", &word->a[lineNum][i]);
+				i++;
+				pch = strtok (NULL, " ");
+			}
+
+			lineNum++;
+		}
+		//printf("%d \n",lineNum);
+		fclose(fr);
+	}
+
+}
 
 
 void copy_2d(float **from_array, float **to_array, int row, int col)
@@ -170,33 +225,81 @@ void copy_2d(float **from_array, float **to_array, int row, int col)
 }
 
 
-void check_1d_f(float *array, int len)
+void check_pri(HMM *word)
 {
 	int i;
+	int len = word->len;
+
 	for(i=0;i<len;++i)
 	{
-		printf("%10.4e\n",array[i]);	
+		printf("%10.4e\n", word->pri[i]);	
 	}
 	printf("\n");
 
 }
 
 
-void check_2d_f(float **array, int row, int col)
+void check_a(HMM *word)
 {
 	int i,j;
+	int row = word->nstates;
+	int col = word->len;
+
 	for(i=0;i<row;++i)
 	{
 		printf("row(%d)\n",i);
 		for(j=0;j<col;++j)
 		{
-			printf("%10.4e\n",array[i][j]);	
+			printf("%10.4e\n",word->a[i][j]);	
 
 		}
 		printf("\n\n");
 	}
 
 }
+
+
+void check_b(HMM *word)
+{
+	int i,j;
+	int row = word->nstates;
+	int col = word->len;
+
+	for(i=0;i<row;++i)
+	{
+		printf("row(%d)\n",i);
+		for(j=0;j<col;++j)
+		{
+			printf("%10.4e\n",word->b[i][j]);	
+
+		}
+		printf("\n\n");
+	}
+
+}
+
+
+
+void free_hmm(HMM *word)
+{
+	int T,i;
+	T = word->len;
+	
+	for(i = 0; i < T ; ++i) {
+		free(word->a[i]);
+		free(word->b[i]);
+	}
+
+	free(word->a);
+	free(word->b);
+	free(word->pri);
+
+}
+
+
+
+
+
 int main(int argc, char*argv[])
 {
 	// 6 config, each has three files to read
@@ -225,9 +328,13 @@ int main(int argc, char*argv[])
 	int job;
 	int Len;
 
-	int debug=0;
+	int debug=1;
+
 	
 	for(job = 0 ; job < 6 ; ++job){
+		HMM *word;
+		word = (HMM*)malloc(sizeof(HMM));
+
 		printf("\n\njob (%d) => ", job);
 
 		Len = getLineNum(files[job*3+2]);	
@@ -236,18 +343,19 @@ int main(int argc, char*argv[])
 		//read B,A,prior
 		puts("Read the following files.");
 		//read_config(files,job,B,A,prior,Len);
-		read_config(files,job,Len);
+		read_config(word,files,job,Len);
 
-		if( debug ){
-			//check_2d_f(b,Len,Len);	
-			//check_2d_f(a,Len,Len);	
-			//check_1d_f(pri,Len);	
+		if( debug && job == 0 ) {
+			puts("a");
+			check_a(word);	
+			puts("b");
+			check_b(word);	
+			puts("pri");
+			check_pri(word);	
 		}
 
-
-
-
-
+		// free memory
+		free_hmm(word);
 	}
 
 
