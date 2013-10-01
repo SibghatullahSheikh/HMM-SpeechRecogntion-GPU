2703d8bddfccb97c97b9994d2c79a50e8c67d5a3
Leiming Yu <leimingyu830@gmail.com>
2013-10-01 01:06:43 -0400

    forward

--- performance/forward/c_fo.c
+++ performance/forward/c_fo.c
@@ -6,35 +6,49 @@
 //#include <time.h>
 #include <sys/time.h>
 
-
 #define BUFSIZE 1000000
 
-void read_config(char **files,int job, float **B, float **A, float *prior, int len);
+float **b;
+float **a;
+float *pri;
+
+
+//void read_config(char **files,int job, float **b, float **a, float *pri, int len);
+void read_config(char **files,int job,int len);
+
 int getLineNum(char *file);
+
 void copy_2d(float **from_array, float **to_array, int row, int col);
-void read_2d(char *file ,float **b, int row ,int col);
+
+void read_2d(char *file ,float **x, int row ,int col);
+
+void read_1d(char *file ,float *x, int len);
+
 void check_2d_f(float **array, int row, int col);
 
-void read_config(char **files,int job, float **B, float **A, float *prior, int len)
+void check_1d_f(float *array, int len);
+
+//void read_config(char **files,int job, float **b, float **a, float *pri, int len)
+void read_config(char **files,int job, int len)
 {
 	// variables
-	int i,j;
+	int i;
 	int N = len;
 
 	// allocate parameters
-	float **b;
+	//float **b;
 	b = (float**)malloc(sizeof(float*)*N);
 	for(i = 0; i < N; ++i) {
 		b[i] = (float*)malloc(sizeof(float)*N);
 	}
 
-	float **a;
+	//float **a;
 	a = (float**)malloc(sizeof(float*)*N);
 	for(i = 0; i < N; ++i) {
 		a[i] = (float*)malloc(sizeof(float)*N);
 	}
 
-	float *pri;
+	//float *pri;
 	pri = (float*)malloc(sizeof(float)*N);
 
 
@@ -44,34 +58,25 @@ void read_config(char **files,int job, float **B, float **A, float *prior, int l
 		// read B
 		if(i == 0){
 			printf("read B[%d][%d] \t\t", N, N);
-			// read matrix
 			read_2d(files[job*3],b,N,N);
-		
-			//if(job == 0){
-			//	check_2d_f(b,N,N);	
-			//}
-
-			//copy_2d(b,B,N,N);
-
 			printf("done!\n");
 		}
 
 		// read A
 		if(i == 1){
 			printf("read A[%d][%d] \t\t", N, N);
+			read_2d(files[job*3+1],a,N,N);
 			printf("done!\n");
 		}
 
 		// read prior
 		if(i == 2){
 			printf("read prior[%d] \t\t", N);
+			read_1d(files[job*3+2],pri,N);
 			printf("done!\n");
 		}
 
-
 	}	
-
-
 
 }
 
@@ -96,7 +101,29 @@ int getLineNum(char *file)
 }
 
 
-void read_2d(char *file ,float **b, int row ,int col)
+void read_1d(char *file ,float *x, int len)
+{
+	int lineNum = 0;
+	char buf[100];
+	
+	FILE *fr;
+	fr = fopen(file, "r");
+	if(fr == NULL) {
+		printf("Can't open %s!\n", file);
+		exit(1);
+	}else{
+		while(fgets(buf, sizeof(buf), fr) != NULL)
+		{
+			sscanf(buf,"%f", &x[lineNum]);
+			lineNum++;
+		}
+		fclose(fr);
+	}
+}
+
+
+
+void read_2d(char *file ,float **x, int row ,int col)
 {
 	int i;
 	int lineNum = 0;
@@ -115,24 +142,21 @@ void read_2d(char *file ,float **b, int row ,int col)
 			pch =  strtok(buf," ");
 			while(pch != NULL)
 			{
-				//if(i < col){
-					sscanf(pch,"%f", &b[lineNum][i]);
-				//}
+				sscanf(pch,"%f", &x[lineNum][i]);
 				i++;
-				pch = strtok (NULL, " ,.-");
-
-
+				pch = strtok (NULL, " ");
 			}
 
 			lineNum++;
 		}
-
-		printf("%d \n",lineNum);
-
+		//printf("%d \n",lineNum);
 		fclose(fr);
 	}
 
 }
+
+
+
 
 void copy_2d(float **from_array, float **to_array, int row, int col)
 {
@@ -146,21 +170,33 @@ void copy_2d(float **from_array, float **to_array, int row, int col)
 }
 
 
+void check_1d_f(float *array, int len)
+{
+	int i;
+	for(i=0;i<len;++i)
+	{
+		printf("%10.4e\n",array[i]);	
+	}
+	printf("\n");
+
+}
+
+
 void check_2d_f(float **array, int row, int col)
 {
 	int i,j;
 	for(i=0;i<row;++i)
 	{
+		printf("row(%d)\n",i);
 		for(j=0;j<col;++j)
 		{
-			printf("%10.4e ",array[i][j]);	
+			printf("%10.4e\n",array[i][j]);	
 
 		}
-		printf("\n");
+		printf("\n\n");
 	}
 
 }
-
 int main(int argc, char*argv[])
 {
 	// 6 config, each has three files to read
@@ -189,11 +225,7 @@ int main(int argc, char*argv[])
 	int job;
 	int Len;
 
-	float **B;
-	float **A;
-	float *prior;
-
-
+	int debug=0;
 	
 	for(job = 0 ; job < 6 ; ++job){
 		printf("\n\njob (%d) => ", job);
@@ -203,7 +235,17 @@ int main(int argc, char*argv[])
 
 		//read B,A,prior
 		puts("Read the following files.");
-		read_config(files,job,B,A,prior,Len);
+		//read_config(files,job,B,A,prior,Len);
+		read_config(files,job,Len);
+
+		if( debug ){
+			//check_2d_f(b,Len,Len);	
+			//check_2d_f(a,Len,Len);	
+			//check_1d_f(pri,Len);	
+		}
+
+
+
 
 
 	}
